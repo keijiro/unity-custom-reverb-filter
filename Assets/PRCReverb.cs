34be58154cba534ec3e6a3772b17dc805ce09f8d
Keijiro Takahashi <keijiro@gmail.com>
2013-07-15 15:32:19 +0900

    Moved DelayLine class to its own source file.

--- Assets/PRCReverb.cs
+++ Assets/PRCReverb.cs
@@ -1,35 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-public class DelayLine
-{
-    float[] buffer;
-    int position;
-
-    public int Length {
-        get { return buffer.Length; }
-    }
-
-    public float Last {
-        get { return buffer [position]; }
-    }
-
-    public DelayLine (int length)
-    {
-        buffer = new float[length];
-    }
-
-    public float Tick (float input)
-    {
-        var last = buffer [position];
-        buffer [position] = input;
-        if (++position >= buffer.Length) {
-            position = 0;
-        }
-        return last;
-    }
-}
-
 public class PRCReverb : MonoBehaviour
 {
 
